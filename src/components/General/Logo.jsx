650e66d1e0305c8a7438be305cd31862079a65c2
polina korsak <polinakorsak@bk.ru>
2023-05-01 14:03:13 +0300

    авели красоту в footer

--- src/components/General/Logo.jsx
+++ src/components/General/Logo.jsx
@@ -1,6 +1,6 @@
 import logoImg from "../../assents/images/logo.png";
 
-const Logo = () => <a className="logo">
+const Logo = () => <a className="logo" href="/">
     <img src={logoImg} alt="DogFood" />
     <span className="logo__text">DogFood</span>
 </a>
